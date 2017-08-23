129f33fa1167292e3c283553f7b1cf8d64415d44
Phil Hachey <phil.hachey@bluespurs.com>
2017-08-23 08:32:10 -0300

    Updated plugin to fill in individual function environment params

--- index.js
+++ index.js
@@ -3,30 +3,43 @@
 const _ = require("lodash");
 
 class ExportEnvWsgi {
-	constructor(serverless, options) {
-		this.serverless = serverless;
-		this.hooks = {
-			"before:wsgi:serve:serve": this.initEnv.bind(this)
-		};
-	}
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.hooks = {
+      "before:wsgi:serve:serve": this.initEnv.bind(this)
+    };
+  }
 
-	initEnv() {
-		const AWS = this.serverless.providers.aws;
+  initEnv() {
+    const AWS = this.serverless.providers.aws;
 
-		return AWS.request("CloudFormation", "describeStacks", {
-			StackName: AWS.naming.getStackName()
-		})
-		.then((response) => {
-			const stack = response.Stacks[0];
+    return AWS.request("CloudFormation", "describeStacks", {
+        StackName: AWS.naming.getStackName()
+      })
+      .then((response) => {
+        const stack = response.Stacks[0];
 
-			_.each(this.serverless.service.provider.environment, (value, key) => {
-				const output = _.find(stack.Outputs, {'OutputKey': key});
-				if(output) {
-					this.serverless.service.provider.environment[output.OutputKey] = output.OutputValue;
-				}
-			});
-		});
-	}
+        _.each(this.serverless.service.provider.environment, (value, key) => {
+          const output = _.find(stack.Outputs, {
+            'OutputKey': key
+          });
+          if (output) {
+            this.serverless.service.provider.environment[output.OutputKey] = output.OutputValue;
+          }
+        });
+
+        _.each(this.serverless.service.functions, (func, func_key) => {
+          _.each(func.environment, (func_env_value, func_env_key) => {
+            const output = _.find(stack.Outputs, {
+              'OutputKey': func_env_key
+            });
+            if (output) {
+              func.environment[output.OutputKey] = output.OutputValue;
+            }
+          });
+        });
+      });
+  }
 }
 
 module.exports = ExportEnvWsgi;
